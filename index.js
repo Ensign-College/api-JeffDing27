01bb30ddff92428809a67ddfc26fc25699d2053e
JeffDing27 <rud300@ensign.edu>
2024-02-26 15:30:15 -0700

    Updated Api

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express'); // express makes APIs- connect fronted to se
 const Redis = require('redis'); //import the Redis Library
 const bodyParser= require('body-parser');
 const cors = require('cors');
-
+const { addOrder, getOrder} = require("./services/orderservice.js");
+const { addOrderItem, getOrderItem} = require("./services/orderItems");
+const fs = require("fs");
+const Schema = JSON.parse(fs.readFileSync("./orderItemSchema.json", "utf8"));
+const Ajv = require("ajv");
+const ajv = new Ajv();
 
 const options = {
     origin:'http://localhost:3000'//allow our frontend to call this backend
@@ -23,6 +28,83 @@ app.listen(port, ()=>{
     redisClient.connect();//this connects to the redis database!!!!!!
     console.log(`API is Listening on port: ${port}`);//emplate literal
 }); //listen for web requests from the frontend and docker
+
+//order
+app.post("/orders", async (req, res)=>{
+    let order = req.body;
+    //order dtails, cinlude product quantity and shipping address
+    let responseStatus = order.productQuantity
+    ? 200
+    : 400 && order.ShippingAddress
+    ? 200
+    : 400;
+
+if (responseStatus ===200){
+    try{
+        // addOrder function to handle order creation in the database
+        await addOrder({ redisClient, order});
+    }catch (error){
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+        return;
+    }
+    }else{
+        res.status (responseStatus);
+        res.send(
+            `Missing one of the following fields: ${exactMatchOrderFields()} ${partiallyMatchOrderFields()}`
+        );
+    }
+    res.status(responseStatus).send();
+});
+
+app.get("/orders/:orderId", async (req, res)=>{
+//get the order from the database
+const orderId = req.params.orderId;
+let order = await getOrder({redisClient, orderId});
+if (order === null){
+    res.status(404).send("Order not found");
+}else{
+    res.json(order);
+}
+});
+//ORDER ITEMS
+app.post("/orderItems", async (req, res)=>{
+try{
+    console.log("Schema:", Schema);
+    const validate = ajv.compile(Schema);
+    const valid = validate(req.body);
+    if (!valid){
+        return res.status(400).json({error: "Invalid request body"});
+    }
+    console.log("Request Body:", req.body);
+
+    //Calling addOrderItem function and storing the result
+    const orderItemId = await addOrderItem({
+        redisClient,
+        orderItem: req.body,
+    });
+
+    //Responding with the result 
+    res
+     .status(201)
+     .json({orderItemId, message: "Order item added successfully"});
+}catch (error) {
+    console.error("Error adding order item:", error);
+    res.status(500).json({error: "Internal server error"});
+}
+});
+
+app.get("/orderItems/:orderItemId", async (req, res)=>{
+    try{
+        const oderItemId = req.paramsorderItemId;
+        const orderItem = await getOrderItem({ redisClient, orderItemId});
+        res.json(orderItem);
+    }catch (error){
+        console.error("Error getting order item:", error);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
 
 app.post('/inventory', async (req,res)=>{ //async means we will await promises
 
